3869c87fbd71e711605df775b1c548fbbc32e829
Mia Seo <tjwlfif@gmail.com>
2023-05-24 16:46:17 +0900

    Modify: useNavigate -> Link 태그로 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,50 @@
 import React from "react";
 import { BsFillCartFill, BsPencilFill } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useUser } from "../context/userContext";
 import Button from "./ui/Button";
 import User from "./User";
 
 export default function Navbar() {
-  const navigate = useNavigate();
   const { user, signin, signout } = useUser();
 
   return (
     <nav>
-      <div className="w-full flex justify-center bg-zinc-300 py-2">
+      <Link className="w-full flex justify-center bg-zinc-300 py-2" to="/">
         <img
           className="w-[100px] rounded-sm"
           src="https://github.com/mia-seo/shu-shu/assets/117281717/31fd8bb1-c580-4594-acdf-a2070e0acdbd"
           alt="shushu"
-          onClick={() => navigate("/")}
         />
-      </div>
+      </Link>
       <div className="flex justify-between items-center px-1 py-3">
         <div className="flex gap-2 text-lg font-semibold">
           {CATEGORIES.map((el, index) => (
-            <button
+            <Link
               className="capitalize hover:text-accent transition-all duration-150"
               key={index}
-              onClick={() => {
-                navigate(`/products/${el}`);
-              }}
+              to={`/products/${el}`}
             >
               {el}
-            </button>
+            </Link>
           ))}
         </div>
         <div className="flex items-center gap-5 text-xl">
           {user && (
-            <button
+            <Link
               className="hover:text-accent transition-all duration-150"
-              onClick={() => navigate("/carts")}
+              to="/carts"
             >
               <BsFillCartFill />
-            </button>
+            </Link>
           )}
           {user && user.isAdmin && (
-            <button
+            <Link
               className="hover:text-accent transition-all duration-150"
-              onClick={() => navigate("/products/new")}
+              to="/products/new"
             >
               <BsPencilFill />
-            </button>
+            </Link>
           )}
           {user ? (
             <div className="flex items-center gap-2">
