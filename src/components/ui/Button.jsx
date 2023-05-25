2a022ff11b9202c1947dcfeb4736896e104ce493
Mia Seo <tjwlfif@gmail.com>
2023-05-25 12:25:49 +0900

    Add: NewProducts 구현 완료

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Button({ text, onClick }) {
+export default function Button({ text, onClick, disabled }) {
   return (
     <button
-      className="text-lg font-semibold bg-brand text-white px-3 py-1 rounded-sm hover:bg-accent transition-all duration-150"
+      className={`text-lg font-semibold bg-brand text-white px-3 py-1 rounded-sm hover:bg-accent transition-all duration-150`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
